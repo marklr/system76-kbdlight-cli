75b987b2bfcb9fa4930647b80118b2cdd8268910
Mark R <mark@mark.org.il>
2020-04-28 14:58:05 +0300

    initial import

---
+++ kblight.py
@@ -0,0 +1,82 @@
+#!/bin/env python3
+import sys
+
+import plac
+import webcolors
+import os
+
+_COLOR_FILTERS = {
+    '#': '',
+    '(': '',
+    ')': '',
+}
+
+LIGHT_CONTROLS = {
+    'left': '/sys/class/leds/system76::kbd_backlight/color_left',
+    'center': '/sys/class/leds/system76::kbd_backlight/color_center',
+    'right': '/sys/class/leds/system76::kbd_backlight/color_right',
+}
+
+def check_light_controls():
+    for light, fpath in LIGHT_CONTROLS.items():
+        if not os.path.exists(fpath):
+            raise FileNotFoundError(f"Light path {fpath} for light {light} does not exist")
+
+
+def parse_color(color):
+    c = str(color).lower().strip()
+    for k, v in _COLOR_FILTERS.items():
+        c = c.replace(k, v)
+
+    hex = None
+    try:
+        rgb = webcolors.hex_to_rgb(c)
+        if rgb:
+            return c
+    except Exception:
+        # probably not a hex already
+        pass
+
+    try:
+        hex = webcolors.name_to_hex(c)
+    except Exception:
+        pass
+
+    if '%' in c:
+        try:
+            hex = webcolors.rgb_percent_to_hex((int(x.strip()) for x in c.split(',')))
+        except Exception:
+            pass
+    else:
+        try:
+            hex = webcolors.rgb_to_hex((int(x.strip()) for x in c.split(',')))
+        except Exception:
+            pass
+
+    return hex[1:] if hex else None
+
+def set_light(name, hex):
+    if name not in LIGHT_CONTROLS:
+        raise NotImplementedError(f"Cannot control light {name}")
+
+    print(f"[+] Setting light {name} to {hex}", file=sys.stdout)
+
+    with open(LIGHT_CONTROLS[name], 'w') as f:
+        f.write(hex.upper() + "\n")
+        f.flush()
+
+def main(color: ("color choice (rgb triplet/color name/hex)", 'positional'),
+         light: ("light to set (right/left/center/all)", 'option', 'l')='all'):
+    check_light_controls()
+    hex = parse_color(color)
+
+    if not hex:
+        print(f"[-] Unrecognized color {color}", file=sys.stderr)
+        return os.EX_NOINPUT
+
+    print(f"[*] Parsed color {color} to hex {hex}", file=sys.stdout)
+    for light in filter(lambda x: light == x or light == 'all', LIGHT_CONTROLS.keys()):
+        set_light(light, hex)
+
+if __name__ == '__main__':
+    plac.call(main)
